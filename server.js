a25228eb646c74311c518ab33116a4d21b6641e8
Daksh MAngal <mangalpradeepkumar@gmail.com>
2024-04-29 21:55:42 +0530

    Changed to mongo db version 2

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require('dotenv').config();
 
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
   res.send("Welcome to my World");
@@ -21,6 +24,6 @@ app.use('/menuItem', menuRoutes)
 
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("listening on port 3000");
 });
